ed6d08aadbc61aeb9051c921d4e61a0e1b5ac34d
Gianluca Esposito <g.esposito16@gmail.com>
2016-01-24 03:39:31 +0100

    Modular in 3... 2... 1...
    Refactoring - step 1: modularization

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -78,38 +78,4 @@ angular.module('codeChallengeApp')
       }
       vm.gameStatus = GameFactory.getStatus();
     });
- }])
-  .factory('GameFactory', ['$rootScope', 'PlayerFactory', function ($rootScope, PlayerFactory){
-  	var gameSettings = {
-  		turn: "X",
-      status: "playing"
-  	};
-    var changeTurn = function(){
-      gameSettings.turn = (gameSettings.turn == "X" ? "O" : "X");
-    };
-  	return {
-  		getTurn: function(){
-  			return gameSettings.turn;
-  		},
-      getStatus: function(){
-        return gameSettings.status;
-      },
-      calculateWinner: function(cell){
-        var winningCombo = PlayerFactory.checkWinningCombo(cell, gameSettings.turn);
-        if (winningCombo != -1) {
-          gameSettings.status = "finished";
-          return winningCombo;
-        } else {
-          var opponent = (gameSettings.turn == "X" ? "O" : "X");
-          PlayerFactory.removeCombosContaining(cell, opponent);
-          changeTurn();
-        }
-        return -1;
-      },
-      resetGame: function(){
-        gameSettings.turn = "X";
-        gameSettings.status = "playing";
-        PlayerFactory.resetCombosAndCells();
-      }
-  	}
  }]);
---
+++ app/scripts/services/player.srvc.js
@@ -0,0 +1,90 @@
+angular.module('codeChallengeApp')
+.factory('PlayerFactory', [function(){
+  	var playersSettings = {
+  		X: {
+	  		name: "Gargaroz",
+        combosLeft: [
+          ["00","01","02"],
+          ["10","11","12"],
+          ["20","21","22"],
+          ["00","10","20"],
+          ["01","11","21"],
+          ["02","12","22"],
+          ["00","11","22"],
+          ["02","11","20"]
+        ],
+        ownedCells: []
+  		},
+  		O: {
+	  		name: "Mister E",
+        combosLeft: [
+          ["00","01","02"],
+          ["10","11","12"],
+          ["20","21","22"],
+          ["00","10","20"],
+          ["01","11","21"],
+          ["02","12","22"],
+          ["00","11","22"],
+          ["02","11","20"]
+        ],
+        ownedCells: []
+  		}
+  	};
+    var recursiveComboRemoval = function (cell, player){
+      playersSettings[player].combosLeft.forEach(function (singleCombo){
+        if (singleCombo.indexOf(cell) != -1) {
+          var comboToRemove = playersSettings[player].combosLeft.indexOf(singleCombo);
+          playersSettings[player].combosLeft.splice(comboToRemove, 1);
+          recursiveComboRemoval(cell, player);
+        }
+      });
+    };
+    var addToOwned = function(cell, player){
+      playersSettings[player].ownedCells.push(cell);
+    }
+    return {
+      setName: function(player, name){
+        playersSettings[player].name = name;
+      },
+      getName: function(player){
+        return playersSettings[player].name;
+      },
+      resetCombosAndCells: function(){
+        console.log("playersSettings = ", playersSettings);
+        angular.forEach(playersSettings, function(player){
+          player.combosLeft = [
+            ["00","01","02"],
+            ["10","11","12"],
+            ["20","21","22"],
+            ["00","10","20"],
+            ["01","11","21"],
+            ["02","12","22"],
+            ["00","11","22"],
+            ["02","11","20"]
+          ];
+          player.ownedCells = [];
+        });
+      },
+      checkWinningCombo: function(cell, player){
+        addToOwned(cell, player);
+        var result = -1;
+        if (playersSettings[player].ownedCells.length < 3) return -1;
+        playersSettings[player].combosLeft.forEach(function(singleCombo){
+          var winningCellsCount = 0;
+          singleCombo.forEach(function(singleComboCell){
+            if (playersSettings[player].ownedCells.indexOf(singleComboCell) == -1) {return;}
+            else {
+              winningCellsCount+=1;
+            }
+            if (winningCellsCount == 3){
+              result = singleCombo;
+            }
+          });
+        });
+        return result;
+      },
+      removeCombosContaining: function(cell, player){
+        recursiveComboRemoval(cell, player);
+      }
+  	}
+}]);
